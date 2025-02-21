c059d481b7f8049363d9bd289ce44036ca70ed8d
David Britt <djmb102@gmail.com>
2025-02-21 17:11:55 -0400

    Add openai nodejs example

---
+++ docs/tutorials/llm/openai.ts
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+const openai = new OpenAI({
+  baseUrl: "https://<nosana-job-id>.node.k8s.prd.nos.ci/v1"
+});
+
+const completion = await openai.chat.completions.create({
+  model: "gpt-4o",
+  messages: [
+    { role: "developer", content: "You are a helpful assistant." },
+    {
+      role: "user",
+      content: "Write a haiku about recursion in programming.",
+    },
+  ],
+  store: true,
+});
+
+console.log(completion.choices[0].message);
