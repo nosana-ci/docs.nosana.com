c39d6363a25d0c4b961541dddf67db0522d118fd
David Britt <djmb102@gmail.com>
2025-03-11 14:33:34 -0400

    Update js script

--- docs/tutorials/llm/openai.ts
+++ docs/tutorials/llm/openai.ts
@@ -1,19 +1,20 @@
+// openai.js
 import OpenAI from "openai";
 const openai = new OpenAI({
-  baseUrl: "https://<nosana-job-id>.node.k8s.prd.nos.ci/v1"
+  baseURL: "https://<nosana-job-id>.node.k8s.prd.nos.ci/v1"
 });
 
+const models = await openai.models.list();
+console.log(models.data);
+
 const completion = await openai.chat.completions.create({
-  // TODO: Change model to qwen model
-  model: "gpt-4o",
+  model: "R1-Qwen-1.5B",
   messages: [
-    { role: "developer", content: "You are a helpful assistant." },
     {
       role: "user",
       content: "Write a haiku about recursion in programming.",
     },
   ],
-  store: true,
 });
 
-console.log(completion.choices[0].message);
+console.log(completion.choices[0].message.content);
